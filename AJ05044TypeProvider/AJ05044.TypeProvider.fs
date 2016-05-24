87b548d4135173bdad87359fe032b8d5e97180f3
AJ <avjmail@gmail.com>
2016-05-25 01:16:01 +0300

    logic fix - loops should be inside method generation

--- AJ05044TypeProvider/AJ05044.TypeProvider.fs
+++ AJ05044TypeProvider/AJ05044.TypeProvider.fs
@@ -8,45 +8,41 @@ open System.Reflection
 [<Literal>]
 let apiUrl = "https://bchi.bigcitieshealth.org/resource/ffnx-yiyc.json"
 type HealthRecord = JsonProvider<apiUrl>
-let healthRecords = HealthRecord.Load(apiUrl)
+let allData = HealthRecord.Load(apiUrl)
 
 [<TypeProvider>]
 type AJProvider (config : TypeProviderConfig) as this =
     inherit TypeProviderForNamespaces ()
     let namespace_ = "BigCitiesHealth"
     let assembly = Assembly.GetExecutingAssembly()
-    let categoriesNames = query {for i in healthRecords do select i.IndicatorCategory } |> Seq.distinct
+    let categoriesNames = query {for i in allData do select i.IndicatorCategory } |> Seq.distinct
     let makeCategoryType (categoryName: string) =
         let category = ProvidedTypeDefinition(assembly, namespace_, categoryName, Some typeof<obj>)
-        
-        let categoryData = healthRecords |> Seq.filter (fun r -> r.IndicatorCategory = categoryName)
+        let categoryData = allData |> Seq.filter (fun r -> r.IndicatorCategory = categoryName)
         let indicatorsNames = query {for i in categoryData do select i.Indicator} |> Seq.distinct
         for indicatorName in indicatorsNames do
-            let indicatorData = categoryData |> Seq.filter (fun r -> r.Indicator = indicatorName)
-            let citiesNames = query {for i in indicatorData do select i.Place} |> Seq.distinct
-            for cityName in citiesNames do
-                
-                
-
-                    category.AddMembersDelayed(fun () -> 
-                        let indicator = ProvidedTypeDefinition(indicatorName, Some typeof<obj>)                
-                        indicator.AddMembersDelayed (fun () -> 
-                            let city = ProvidedTypeDefinition(cityName, Some typeof<obj>)                
-                            city.AddMembersDelayed (fun () -> 
-                                
-
-                                    let measurement = 
-                                        let value = "measurement value"
-                                        let property = ProvidedProperty(
-                                                        propertyName = "Measurement", 
-                                                        propertyType = typeof<string>, 
-                                                        IsStatic=true,
-                                                        GetterCode= (fun args -> <@@ value @@>))
-                                        property
-                                    [measurement])
-
-                            [city])
-                        [indicator])
+            category.AddMembersDelayed(fun () -> 
+                let indicator = ProvidedTypeDefinition(indicatorName, Some typeof<obj>)           
+                let indicatorData = categoryData |> Seq.filter (fun r -> r.Indicator = indicatorName)
+                let citiesNames = query {for i in indicatorData do select i.Place} |> Seq.distinct
+                for cityName in citiesNames do
+                    indicator.AddMembersDelayed (fun () ->  
+                        let city = ProvidedTypeDefinition(cityName, Some typeof<obj>)           
+                        let cityData = indicatorData |> Seq.filter (fun r -> r.Place = cityName)
+                        let yearsNames = query {for i in cityData do select i.Year} |> Seq.distinct
+                        for yearName in yearsNames do
+                            city.AddMembersDelayed (fun () ->  
+                                let measurement = 
+                                    let value = "measurement value"
+                                    let property = ProvidedProperty(
+                                                    propertyName = "Measurement", 
+                                                    propertyType = typeof<string>, 
+                                                    IsStatic=true,
+                                                    GetterCode= (fun args -> <@@ value @@>))
+                                    property
+                                [measurement])
+                        [city])
+                [indicator])
         category
     let dataTypes = [for c in categoriesNames -> makeCategoryType c]
     do this.AddNamespace(namespace_, dataTypes)
