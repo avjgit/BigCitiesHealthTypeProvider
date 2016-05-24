1038d5372818445289c0271efcd1a03be812fab6
AJ <avjmail@gmail.com>
2016-05-24 23:02:59 +0300

    types for indicators

--- AJ05044TypeProvider/AJ05044.TypeProvider.fs
+++ AJ05044TypeProvider/AJ05044.TypeProvider.fs
@@ -14,25 +14,37 @@ let healthRecords = HealthRecord.Load(apiUrl)
 [<TypeProvider>]
 type AJProvider (config : TypeProviderConfig) as this =
     inherit TypeProviderForNamespaces ()
-    
     let namespace_ = "BigCitiesHealth"
     let assembly = Assembly.GetExecutingAssembly()
-
+    
     let makeCategoryType (categoryName: string) =
-        let t = ProvidedTypeDefinition(assembly, namespace_, categoryName, Some typeof<obj>)
-        let prop = ProvidedProperty(propertyName = "SomeProperty", 
-                                    propertyType = typeof<string>, 
-                                    IsStatic=true,
-                                    GetterCode= (fun args -> <@@ "demo value" @@>))
-        t.AddMember prop
-        t
+        let categorie = ProvidedTypeDefinition(assembly, namespace_, categoryName, Some typeof<obj>)
 
-    let categories = query {for i in healthRecords do 
-                                select i.IndicatorCategory 
+        let indicators = query {for i in healthRecords do
+                                where (i.IndicatorCategory = categoryName) 
+                                select i.Indicator 
                                 distinct}
 
-    let dataTypes = [for i in categories -> makeCategoryType i]
-    
+        for i in indicators do
+            categorie.AddMembersDelayed(fun () -> 
+                let indicator = ProvidedTypeDefinition(i, Some typeof<obj>)
+                indicator.AddMembersDelayed (fun () -> 
+                    let indicatorValue = 
+                        let iValue = "indicator value"
+                        let p = ProvidedProperty(
+                                    propertyName = "Value", 
+                                    propertyType = typeof<string>, 
+                                    IsStatic=true,
+                                    GetterCode= (fun args -> <@@ iValue @@>))
+                        p
+                    [indicatorValue])
+                [indicator])
+        categorie
+
+    let categories = query {for i in healthRecords do 
+                            select i.IndicatorCategory 
+                            distinct}
+    let dataTypes = [for c in categories -> makeCategoryType c]
     do this.AddNamespace(namespace_, dataTypes)
 
 [<assembly:TypeProviderAssembly>]
